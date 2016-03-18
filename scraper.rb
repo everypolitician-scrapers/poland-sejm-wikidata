ca54c625c67597a3db42c530ceff3dd6960f5298
Tony Bowden <tony@tmtm.com>
2016-03-18 07:34:59 +0100

    Take in 250s

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,7 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/poland-sejm-wikipedia', column: 'wikipedia__pl')
-EveryPolitician::Wikidata.scrape_wikidata(names: { pl: names })
-warn EveryPolitician::Wikidata.notify_rebuilder
+names.shuffle.each_slice(250) do |sliced|
+  EveryPolitician::Wikidata.scrape_wikidata(names: { pl: names })
+end
 
