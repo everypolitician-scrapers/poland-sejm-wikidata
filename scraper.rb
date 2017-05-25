53d2335ffc3be93416d3d4d088212024192086cf
Tony Bowden <tony@tmtm.com>
2017-05-25 10:51:01 +0100

    Also find via Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -4,5 +4,12 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/poland-sejm-wikipedia', column: 'wikipedia__pl')
-EveryPolitician::Wikidata.scrape_wikidata(names: { pl: names })
+
+# Find all relevant P39s
+query = <<EOS
+  SELECT DISTINCT ?item WHERE { ?item wdt:P39 wd:Q19269361 }
+EOS
+p39s = EveryPolitician::Wikidata.sparql(query)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { pl: names })
 
